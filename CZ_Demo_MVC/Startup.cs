326712cb53db291ac24aee9a46c86c70f58c3aa1
chen <chen@DESKTOP-0O3VERN>
2016-07-28 21:29:31 +0800

    Add project files.

---
+++ CZ_Demo_MVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CZ_Demo_MVC.Startup))]
+namespace CZ_Demo_MVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
